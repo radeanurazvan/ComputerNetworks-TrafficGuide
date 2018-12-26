7bf9b9321d64879216f50ff3200ee3d4b82f7e47
Radeanu Razvan <radeanu.razvan99@gmail.com>
2018-12-26 22:00:18 +0200

    Fixed build shell

--- Server/build.sh
+++ Server/build.sh
@@ -2,5 +2,6 @@
 
 g++ -g -I ../ -pthread -fpermissive      \
     -o ../output/TrafficGuideServer.exe  \
+    ../Kernel/Functional/*               \
     Infrastructure/*                     \
     Entry.cpp
